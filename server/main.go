385b29d233ecf3d019ed2a583025c3ed5c726055
Tristan Timblin <tatimblin@gmail.com>
2024-01-31 22:57:51 -0800

    use SomeQuery as event

--- server/main.go
+++ server/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"spotify-stream-player/server/modules/broker"
 	"spotify-stream-player/server/modules/player"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	log.Print("starting server...")
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
 
 	broker := broker.NewServer()
 
@@ -20,15 +26,14 @@ func main() {
 	go func() {
 		for {
 			time.Sleep(time.Second * 2)
-			eventString := fmt.Sprintf("this time is %v", time.Now())
-			log.Println("Receiving Event")
-			broker.Notifier <- []byte(eventString)
 
 			someUser, err := player.SomeQuery()
 			if err != nil {
 				log.Fatalf("could not get user")
 			}
-			log.Print(someUser)
+
+			log.Println("Receiving Event")
+			broker.Notifier <- []byte(someUser.DisplayName)
 		}
 	}()
 
