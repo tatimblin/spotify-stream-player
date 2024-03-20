aabf5823bc7cf131466b22596646b12650438453
Tristan Timblin <tatimblin@gmail.com>
2024-03-19 22:24:21 -0700

    try http server

--- client/index.ts
+++ client/index.ts
@@ -1,8 +1,17 @@
-const server = Bun.serve({
+import { withHtmlLiveReload } from "bun-html-live-reload";
+
+export default withHtmlLiveReload({
   port: 3000,
   fetch(req) {
-    return new Response("Hello Bunner!");
+    const path = new URL(req.url).pathname;
+
+    switch (path) {
+      case "/":
+        return new Response(Bun.file("./dist/index.html"), {
+          headers: { "Content-Type": "text/html" },
+        });
+      default:
+        return new Response(Bun.file("./" + path));
+    }
   },
 });
-
-console.log(`Listening on port ${server.port}`);
