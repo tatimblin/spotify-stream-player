e1143898af8cd59edd457219d9e7232abddb52a6
Tristan Timblin <tatimblin@gmail.com>
2024-03-25 22:07:28 -0700

    add eventsource and html template

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,7 +1,88 @@
+interface EventResponse extends Event {
+  data: Response,
+}
+
+declare global {
+  interface EventSourceEventMap {
+    ['my-event']: MessageEvent<EventResponse>;
+  }
+}
+
+interface Link {
+  label: string,
+  url: string,
+}
+
+interface Response {
+  track: Link,
+  album: Link,
+  artist: Link,
+  cover: string,
+  progress: number,
+  duration: number,
+  preview: string,
+}
+
 class SpotifyPlayer extends HTMLElement {
+  #source: string;
+  #data: Response;
+
+  constructor() {
+    super();
+
+    this.#source = "http://localhost:8080/";
+  }
+
   connectedCallback() {
-    const now = new Date();
-    this.textContent = now.toLocaleDateString() + "hey there mr dog";
+    const template = document.getElementById('my-template');
+    console.log(template)
+    const evtSource = new EventSource(this.#source);
+
+    evtSource.onmessage = (event: Event) => {
+      const messageEvent = (event as MessageEvent);
+      this.#data = JSON.parse(messageEvent.data);
+      this.render()
+    }
+
+    evtSource.addEventListener
+  }
+
+  render() {
+    this.innerHTML = `
+        <style>
+          .details {
+            display: flex;
+            gap: 8px;
+            align-items: center;
+          }
+          .progress {
+            padding: 16px 32px;
+          }
+          .progress-bar {
+            width: 100%;
+          }
+        </style>
+        <div class="details">
+          <img src="${this.#data.cover}"/>
+          <div>
+            <span>${this.#data.track.label}</span><br/>
+            <span>${this.#data.artist.label} – ${this.#data.album.label}</span>
+          </div>
+        </div>
+        <div class="progress">
+          <progress
+            class="progress-bar"
+            value="${this.#data.progress}"
+            max="${this.#data.duration}"
+          >
+              ${this.getProgress()}%
+          </progress>
+        </div>
+      `;
+  }
+
+  getProgress(): number {
+    return Math.floor((this.#data.progress / this.#data.duration) * 100);
   }
 }
 
