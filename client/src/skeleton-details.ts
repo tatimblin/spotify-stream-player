3922dfd30d9d4219619782b3f3886ac8b5c154b7
Tristan Timblin <tatimblin@gmail.com>
2025-08-05 18:32:08 -0700

    add music note svg

--- client/src/skeleton-details.ts
+++ client/src/skeleton-details.ts
@@ -7,7 +7,9 @@ export interface SkeletonDetailsInterface {
 export default function SkeletonDetails(): string {
   return `
     <div class="${classes.details} ${classes.skeleton}">
-      <div class="${classes.album}">&nbsp;</div>
+      <div class="${classes.album}">
+        <img src="/placeholder.svg" alt="Music placeholder" style="width: 100%; height: 100%; object-fit: cover; opacity: 0.3;" />
+      </div>
       <div class="${classes.details_text}">
         <div class="${classes.head}">&nbsp;</div>
         <div class="${classes.rib}">&nbsp;</div>
